3ab6d19b276124ce56deb09500ed4e15dfece0b6
a0172418 <43873746+a0172418@users.noreply.github.com>
2018-10-11 15:42:15 +0900

    変更

--- Paiza/paiza-4-08-2.py
+++ Paiza/paiza-4-08-2.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 
 '''
 演習課題「おみくじを作ろう」
@@ -21,7 +21,12 @@
 # おみくじプログラム
 
 import random
-line = input().rstrip()
+line = input().rstrip().split(",")
+num=len(line)
+print(line)
+omikuji=random.randrange(num)
+print(line[omikuji])
+
 
 # 今回は自力で全部書いてみよう！
 
